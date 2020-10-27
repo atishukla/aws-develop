f7b1461a584cc161d5b68db24bda8886f6f87bd2
atishukla <atishayshukla@gmail.com>
2020-10-27 09:53:25 +0100

    s3 upload event and email

---
+++ lambda/s3-lambda-event.py
@@ -0,0 +1,52 @@
+import json
+import urllib.parse
+import boto3
+
+print('Loading function')
+
+s3 = boto3.client('s3')
+
+
+def lambda_handler(event, context):
+    # 1 - Get the bucket name
+    bucket = event['Records'][0]['s3']['bucket']['name']
+
+    # 2 - Get the file/key name
+    key = urllib.parse.unquote_plus(
+        event['Records'][0]['s3']['object']['key'], encoding='utf-8')
+
+    try:
+        # 3 - Fetch the file from S3
+        response = s3.get_object(Bucket=bucket, Key=key)
+
+        # 4 - Deserialize the file's content
+        text = response["Body"].read().decode()
+        data = json.loads(text)
+
+        # 5 - Print the content
+        print(data)
+
+        # 6 - Parse and print the transactions
+        transactions = data['transactions']
+        for record in transactions:
+            print(record['transType'])
+
+        if record['transType'] == 'REFUND':
+            sendSMS()
+
+        return 'Success!'
+
+    except Exception as e:
+        print(e)
+        raise e
+
+
+def sendSMS():
+    print('Sending email ......')
+    client = boto3.client('sns')
+    client.publish(
+        TopicArn='arn:aws:sns:eu-west-2:879275023674:NFRCheck',
+        Message='The transaction is refund',
+        Subject='The transaction is refund'
+    )
+    print('Done Sending email ......')
